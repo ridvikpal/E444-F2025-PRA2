e1f61d9ca0e52b84ea4a718c7bcc2f99f35aa250
Ridvik Pal <ridvikpal@gmail.com>
2025-09-19 23:04:03 -0400

    Added support for email address in the form.

--- hello.py
+++ hello.py
@@ -11,7 +11,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from datetime import datetime, timezone
 
 app = Flask(__name__)
@@ -21,8 +21,15 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
+def has_at_character(form, field):
+    if '@' not in field.data:
+        raise ValidationError(f"Please include an '@' in the email address. '{field.data}' is missing an '@'.")
+
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField(
+        "What is your UofT Email address?", validators=[DataRequired(), has_at_character]
+    )
     submit = SubmitField("Submit")
 
 
@@ -31,12 +38,20 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get("name")
+        old_email = session.get("email")
+
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
+        if old_email is not None and old_email != form.email.data:
+            flash("Looks like you have changed your email!")
+
         session["name"] = form.name.data
+        session["email"] = form.email.data
+
         form.name.data = ""
+        form.email.data = ""
         return redirect(url_for("index"))
-    return render_template("index.html", form=form, name=session.get("name"))
+    return render_template("index.html", form=form, name=session.get("name"), email=session.get("email"))
 
 
 @app.route("/user/<name>")
