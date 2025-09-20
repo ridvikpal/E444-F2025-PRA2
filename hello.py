abe6d3c9186a1eba5711da440d53723f15dab754
Ridvik Pal <ridvikpal@gmail.com>
2025-09-19 22:32:41 -0400

    Added basic input field for name in index.html as defined in example 4-2, 4-3, and 4-4.

--- hello.py
+++ hello.py
@@ -1,23 +1,38 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from datetime import datetime, timezone
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+app.config["SECRET_KEY"] = "hard to guess string"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-@app.route("/")
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html", current_time=datetime.now(timezone.utc))
+    name = None
+    form = NameForm()
+    if form.validate():
+        name = form.name.data
+        form.name.data = ""
+    return render_template("index.html", form=form, name=name)
 
 
 @app.route("/user/<name>")
 def user(name):
-    return render_template("user.html", name=name, current_time=datetime.now(timezone.utc))
+    return render_template(
+        "user.html", name=name, current_time=datetime.now(timezone.utc)
+    )
 
 
 @app.errorhandler(404)
