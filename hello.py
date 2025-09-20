e4087e68c1c15108a7764032928df9da634d1c4d
Ridvik Pal <ridvikpal@gmail.com>
2025-09-19 21:54:49 -0400

    Updated code to show the current date and time using flask-moment as described in example 3-11, 3-12 and 3-13.

--- hello.py
+++ hello.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime, timezone
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
-@app.route("/") # adding this so the base url also routes to /index
-@app.route("/index")
+@app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", current_time=datetime.now(timezone.utc))
 
 
 @app.route("/user/<name>")
